f89e398d72e6f85552bc331504f9b4329eb46876
mobaibai <puresilent.com@gmail.com>
2023-06-20 18:04:51 +0800

    🎉 init: 模板初始化

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  onLaunch() {
+    console.log('App Launch')
+  },
+  onShow() {
+    console.log('App Show')
+  },
+  onHide() {
+    console.log('App Hide')
+  },
+}
+</script>
+
+<style>
+.uno-start {
+  --un: 0;
+}
+/* unocss 代码生成在这 */
+.uno-end {
+  --un: 0;
+}
+
+view,:before,:after {
+  border-width: 0;
+  border-style: solid;
+  border-color: #e5e7eb
+}
+</style>
